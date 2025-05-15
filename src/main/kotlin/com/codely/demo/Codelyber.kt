9fa8c10c4b8c2fb806482a0a03afff5907b98642
Eduardo A.F. <eacevedof@yahoo.es>
2025-05-15 09:44:58 +0200

    het behandelen van null-waarden zonde functie getNullHandledString

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -7,7 +7,7 @@ fun main() {
     println("Voer alstublieft een datum in met het formaat: YYYY-MM-DD")
 
     // in geval van een fout retorneer de functie een string: Geen waarde opgegeven
-    val inputString = getNullHandledString(readLine())
+    val inputString = readLine() ?: "Geen waarde opgegeven"
     if (inputString.isEmpty()) {
         println("De ingevoerde datums is ongeldig")
         exitProcess(1)
@@ -19,8 +19,4 @@ fun main() {
     val currentDate = LocalDate.now()
     val difference = Period.between(input, currentDate)
     println("Het verschil tussen data $input en $currentDate is ${difference.years} jaar")
-}
-
-fun getNullHandledString(text: String?): String {
-    return text ?: "Geen waarde opgegeven"
 }
