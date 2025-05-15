6814dd9c5d29b60912bf43e67d21eb39fe7daf48
Eduardo A.F. <eacevedof@yahoo.es>
2025-05-15 10:07:30 +0200

    het behandelen van null-waarden met takeUnless en let context

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -1,22 +1,19 @@
 package com.codely.demo
 import java.time.LocalDate
 import java.time.Period
-import kotlin.system.exitProcess
 
 fun main() {
     println("Voer alstublieft een datum in met het formaat: YYYY-MM-DD")
 
-    // in geval van een fout retorneer de functie een string: Geen waarde opgegeven
-    val inputString = readLine() ?: "Geen waarde opgegeven"
-    if (inputString.isEmpty()) {
-        println("De ingevoerde datums is ongeldig")
-        exitProcess(1)
+    //uitvoer let context tenzij readLine() null teruggeeft
+    readLine().takeUnless {
+        it.isNullOrEmpty()
+    }?.let {
+        val input = LocalDate.parse(it)
+        println("U heeft de datum $input ingevoerd")
+
+        val currentDate = LocalDate.now()
+        val difference = Period.between(input, currentDate)
+        println("Het verschil tussen data $input en $currentDate is ${difference.years} jaar")
     }
-
-    val input = LocalDate.parse(inputString)
-    println("U heeft de datum $input ingevoerd")
-
-    val currentDate = LocalDate.now()
-    val difference = Period.between(input, currentDate)
-    println("Het verschil tussen data $input en $currentDate is ${difference.years} jaar")
 }
