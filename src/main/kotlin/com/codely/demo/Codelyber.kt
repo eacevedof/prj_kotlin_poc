5d1aa2647cb942e72194ebc07a88c3d1e18fe0f8
Eduardo A.F. <eacevedof@yahoo.es>
2025-05-15 07:47:52 +0200

    data invoeren via de console

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -1,7 +1,8 @@
 package com.codely.demo
+import java.time.LocalDate
 
 fun main() {
     println("Please enter a date with the format YYYY-MM-DD:")
-    val input = readLine()
-    println("You wrote $input")
+    val input = LocalDate.parse(readLine())
+    println("You wrote $input ${input.dayOfWeek}")
 }
